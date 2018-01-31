64faa1e0d87c12aae893909e5900883bcaf33b64
PotatoIcyDgx1 <gershie@yahoo.com>
2018-01-30 22:52:19 -0800

    Javascript code cleanup (including removal of slow map recentering along route)

--- script/main.js
+++ script/main.js
@@ -1,4 +1,9 @@
 var debug = window.location.href.endsWith('debug');
+var points = [];
+var origin;
+var iconsize;
+var iconanchor;
+
 function keydown(event) {
 	if (event.keyCode == 39) {
 		popupNextPoint();
@@ -12,75 +17,28 @@ function keydown(event) {
 		closePopup();
 	}
 }
-var future_lat;
-var future_lng;
-var points = [];
-function recentre(lat, lng) {
-	map.panTo(new google.maps.LatLng(lat, lng));
+
+function initMap() {
+	google.charts.load('current', {packages: ['corechart']});
+	google.setOnLoadCallback(drawMap);
 }
-function relocateslow(relocationpoints, i, after) {
-	if (i < relocationpoints.length) {
-		window.setTimeout(function() { recentre(relocationpoints[i].lat, relocationpoints[i].lng) }, 100*i);
-	} else {
-		window.setTimeout(after, 100*relocationpoints.length);
-	}
-}
-function closePopup() {
-	centre = map.getCenter();
-	$('#popup').hide('fade');
-	$('#map').removeClass('open');
-	google.maps.event.trigger(map, 'resize');
-	map.panTo(centre);
+function drawMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: {lat: 47, lng: -91},
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		keyboardShortcuts: false
+	});
+
+	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1qSjDuGdj6l9aj-ghjLUjHrESR5E4NfUIVvnlb0PPmzE/gviz/tq?sheet=Actual&tq=select%20A,B,C,D,E,F,G,H,I');
+	query.send(receiveData);
 }
 function changeZoom(delta) {
 	map.setZoom(map.getZoom() + delta);
 }
-function popupNextPoint() {
-	popupAnotherPoint(function(i) { return i === null ? 0 : i + 1; });
-}
-function popupPrevPoint() {
-	popupAnotherPoint(function(i) { return i === null ? points.length - 1 : i - 1; } );
-}
-function popupAnotherPoint(other) {
-	point_index = $('#popup').attr('point_index')
-	if (point_index) {
-		point_index = parseInt(point_index);
-	} else {
-		point_index = null;
-	}
-	another_point = null;
-	relocationpoints = [];
 
-	for (point_index = other(point_index); !another_point && point_index >= 0 && point_index < points.length; point_index = other(point_index)) {
-		checkpoint = points[point_index];
-		//relocationpoints.push(checkpoint);
-		//recentre(checkpoint.lat, checkpoint.lng);
-		if (checkpoint.icon) {
-			another_point = checkpoint;
-			popupPoint(point_index);
-		}
-	}
-	if (!another_point) {
-		closePopup();
-		$('#popup').attr('point_index', '');
-	}
-	//relocateslow(relocationpoints, i, function() { popupPoint(i); });
-}
 function popupPoint(point_index) {
 	point = points[point_index];
-	//next_point = null;
-	//while (!next_point && ++i < points.length) {
-	//	checkpoint = points[i];
-	//	recentre(checkpoint.lat, checkpoint.lng);
-	//}
-	//next_point = ((i + 1) < points.length) ? points[i + 1] : null;
-
-	//next = $('#next');
-	//if (!next_point) {
-	//	$('#next').hide();
-	//} else {
-	//	$('#next').show();
-	//}
 	
 	var location;
 	var datetime;
@@ -95,7 +53,7 @@ function popupPoint(point_index) {
 	if (debug) {
 		location += ' (#' + point.data_index.toString() + ')';
 	}
-	$('#location').html(location);// + ' - ' + (point.data_index).toString());
+	$('#location').html(location);
 	$('#datetime').html(datetime);
 	var content = '';
 	if (point.comments) {
@@ -119,8 +77,42 @@ function popupPoint(point_index) {
 	$('#popup').show('fade');
 	$('#map').addClass('open');
 	google.maps.event.trigger(map, 'resize');
-	recentre(point.lat, point.lng);
-	//setTimeout(function() { recentre(point.lat, point.lng); }, 100);
+	map.panTo(new google.maps.LatLng(point.lat, point.lng));
+}
+function closePopup() {
+	centre = map.getCenter();
+	$('#popup').hide('fade');
+	$('#map').removeClass('open');
+	google.maps.event.trigger(map, 'resize');
+	map.panTo(centre);
+}
+function popupNextPoint() {
+	popupAnotherPoint(function(i) { return i === null ? 0 : i + 1; });
+}
+function popupPrevPoint() {
+	popupAnotherPoint(function(i) { return i === null ? points.length - 1 : i - 1; } );
+}
+function popupAnotherPoint(other) {
+	point_index = $('#popup').attr('point_index')
+	if (point_index) {
+		point_index = parseInt(point_index);
+	} else {
+		point_index = null;
+	}
+	another_point = null;
+	relocationpoints = [];
+
+	for (point_index = other(point_index); !another_point && point_index >= 0 && point_index < points.length; point_index = other(point_index)) {
+		checkpoint = points[point_index];
+		if (checkpoint.icon) {
+			another_point = checkpoint;
+			popupPoint(point_index);
+		}
+	}
+	if (!another_point) {
+		closePopup();
+		$('#popup').attr('point_index', '');
+	}
 }
 function getPopupPoint(point_index) {
 	return function()
@@ -129,13 +121,6 @@ function getPopupPoint(point_index) {
 	};
 }
 
-var origin;
-var size3x3;
-var size5x5;
-var size12x12;
-var anchor3x3;
-var anchor5x5;
-var anchor12x12;
 
 function displayPoint(point_index, polyline, map) {
 
@@ -150,17 +135,8 @@ function displayPoint(point_index, polyline, map) {
 	}
 
 	if (point.icon) {
-		icon = point.icon.endsWith(".svg") || point.icon.endsWith(".png") ? point.icon :
-			"3x3black.png";
-	} else {
-		icon = "3x3black.png";
-	}
-
-	if (point.icon) {
-		iconsize = icon.endsWith(".svg") || icon.endsWith(".png") ? size12x12 : size5x5;
-		iconanchor = icon.endsWith(".svg") || icon.endsWith(".png") ? anchor12x12 : anchor5x5;
 		iconobject = {
-				url : 'icon/' + icon,
+				url : 'icon/' + point.icon,
 				size : iconsize,
 				anchor : iconanchor,
 				origin : origin,
@@ -189,11 +165,9 @@ function displayPoint(point_index, polyline, map) {
 
 	return polyline;
 }
-
 function displayPoints() {
 	var polyline = null;
 	for (var i = 0; i < points.length; i += 1) {
-		//setTimeout(function() { $('div#loading').html('Loading ' + (i + 1) + ' of ' + points.length) }, 0);
 		polyline = displayPoint(i, polyline, map);
 	}
 	$('input#start').attr('disabled', false);
@@ -206,13 +180,8 @@ function receiveData(response) {
 	datatableNumberOfRows = datatable.getNumberOfRows();
 	
 	origin = new google.maps.Point(0, 0);
-	size3x3 = new google.maps.Size(3, 3);
-	size5x5 = new google.maps.Size(5, 5);
-	size12x12 = new google.maps.Size(20, 20);
-	anchor3x3 = new google.maps.Point(1, 1);
-	anchor5x5 = new google.maps.Point(2, 2);
-	anchor12x12 = new google.maps.Point(10, 10);
-	
+	iconsize = new google.maps.Size(20, 20);
+	iconanchor = new google.maps.Point(10, 10);
 
 	var exclude;
 
@@ -244,24 +213,4 @@ function receiveData(response) {
 		points.push(point);
 	}
 	displayPoints();
-}
-
-
-function initMap() {
-	google.charts.load('current', {packages: ['corechart']});
-
-	google.setOnLoadCallback(drawMap);
-}
-
-function drawMap() {
-
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
-		center: {lat: 47, lng: -91},
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		keyboardShortcuts: false
-	});
-
-	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1qSjDuGdj6l9aj-ghjLUjHrESR5E4NfUIVvnlb0PPmzE/gviz/tq?sheet=Actual&tq=select%20A,B,C,D,E,F,G,H,I');
-	query.send(receiveData);
 }
